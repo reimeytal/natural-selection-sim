41cc94ed48344bfe0efa60f6b60651c959ccf2d3
reimeytal <rei12.meytal11@gmail.com>
2020-07-22 13:12:21 +0300

    Created array of food and added each piece of food to it in its contructor

--- naturalselection/food/Food.java
+++ naturalselection/food/Food.java
@@ -5,15 +5,19 @@ import naturalselection.vector.Vector;
 public class Food{
   private Vector location;
   private boolean isEaten;
+  private int index;
+  public static Food[] food = new Food[175]; //Change amount of food here
+  public static int counter = 0;
+
+
   public Food(int x, int y){
     location = new Vector((short)x, (short) y);
     isEaten = false;
-  }
-  public boolean getIsEaten(){
-    return isEaten;
-  }
-  public void eat(){
-    this.isEaten = true;
+
+    //Adding to food array
+    food[counter] = this;
+    index = counter;
+    counter++;
   }
   public void reset(int x, int y){
     this.isEaten = false;
@@ -21,5 +25,13 @@ public class Food{
   }
   public Vector getLocation(){
     return location;
+  }
+  public boolean tryEat(){
+    if(!isEaten){
+      isEaten = true;
+      return true;
+    } else{
+      return false;
+    }
   }
 }
